00e7f417ad314a17380d36a7da903ce5bae4ca18
evealexis <alicia.e.alexis@gmail.com>
2024-07-11 01:04:59 +0100

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="page-content">
+      <h1>Welcome to Christmas Website!</h1>
+      <p>
+        Christmas is the most wonderful time of the year.
+        <br />
+        <br />
+        Suspendisse potenti. Vestibulum aliquet ante nibh, quis fermentum odio
+        pulvinar id. Mauris ornare ultricies placerat. Quisque vel gravida eros.
+        Duis quis risus eget lorem ultrices pellentesque sed et odio.
+        Pellentesque mattis, elit eu tristique dapibus, leo ante fringilla mi,
+        in placerat augue ipsum eu ex. Integer vestibulum a diam at dignissim.
+        Sed non magna lorem. Fusce elementum euismod mi. Vivamus in consequat
+        eros. Praesent sagittis augue et eros fermentum, sed efficitur lacus
+        condimentum. Duis efficitur dignissim enim, non blandit lectus vehicula
+        non. Donec vulputate fringilla dolor, nec pharetra ex tristique ut.
+        Phasellus facilisis pulvinar vulputate. Ut ac nunc quis risus cursus
+        dapibus. Curabitur sagittis sollicitudin dictum. Phasellus iaculis
+        sapien et tincidunt maximus. Aliquam ante tellus, semper at tellus
+        vitae, hendrerit accumsan ante. Vivamus vitae lorem nec metus fringilla
+        tempus eget vel quam. Ut tincidunt porta posuere.
+        <br />
+        <br />
+        Aliquam non facilisis erat, sed ultricies nisi. Sed eget hendrerit nisi,
+        id pellentesque sapien. Mauris in consequat libero. Maecenas viverra
+        risus sed velit venenatis pretium. Sed nunc sapien, finibus quis nunc a,
+        lacinia lobortis est. Sed vel mollis ante. Aliquam erat volutpat. Aenean
+        a libero quis elit ultrices varius. In sagittis ultricies neque ut
+        facilisis. Integer consectetur dui hendrerit, bibendum est nec, semper
+        ligula. Suspendisse aliquet massa varius, finibus urna quis, fringilla
+        elit. Cras at turpis nec magna gravida sagittis. Praesent commodo felis
+        sit amet consequat molestie. Sed dapibus rutrum quam, eget rhoncus nisi
+        tempus sit amet. Quisque non nibh enim. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Phasellus mattis bibendum est, Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Quisque et dapibus
+        leo. Morbi ullamcorper, ipsum sodales finibus volutpat, turpis leo
+        aliquet ex, a tempor arcu erat sagittis ante. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        <br />
+        <br />
+        Aliquam non facilisis erat, sed ultricies nisi. Sed eget hendrerit nisi,
+        id pellentesque sapien. Mauris in consequat libero. Maecenas viverra
+        risus sed velit venenatis pretium. Sed nunc sapien, finibus quis nunc a,
+        lacinia lobortis est. Sed vel mollis ante. Aliquam erat volutpat. Aenean
+        a libero quis elit ultrices varius. In sagittis ultricies neque ut
+        facilisis. Integer consectetur dui hendrerit, bibendum est nec, semper
+        ligula. Suspendisse aliquet massa varius, finibus urna quis, fringilla
+        elit. Cras at turpis nec magna gravida sagittis. Praesent commodo felis
+        sit amet consequat molestie. Sed dapibus rutrum quam, eget rhoncus nisi
+        tempus sit amet. Quisque non nibh enim. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Phasellus mattis bibendum est, Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Quisque et dapibus
+        leo. Morbi ullamcorper, ipsum sodales finibus volutpat, turpis leo
+        aliquet ex, a tempor arcu erat sagittis ante. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+    </div>
+  );
+}
+
+export default Content;
