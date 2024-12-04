e5e8c9416c9b027f32e0ed28327c7b78d8a8f40a
evealexis <alicia.e.alexis@gmail.com>
2024-12-04 21:02:24 +0000

    Deleted unnecessary files and directories

--- Backend/models/activity.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-const activitySchema = new Schema({
-    description: { type: String, required: true }
-});
--- Frontend/src/components/api/Random.jsx
+++
@@ -1,9 +0,0 @@
-
-const Random = ({ randAct }) => {
-
-    return (
-        <p>{randAct.description}</p>
-    )
-};
-
-export default Random;
